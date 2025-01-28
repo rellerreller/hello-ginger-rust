e9a4c12dc0e9524441a83344fb5bb24098e7dfb2
Nathan Reller <Nathan@GingerCybersecurity.com>
2025-01-28 16:41:02 +0000

    Initial Project with Actix Web

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/file")]
+async fn get_file() -> impl Responder {
+    let message: String = std::fs::read_to_string("/etc/hello.html")
+        .unwrap_or("Unable to open file".to_string());
+    HttpResponse::Ok().body(message)
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .service(get_file)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
+}
